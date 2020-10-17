3fcdae939a75ccd3390eb9349dd20bad5fc95380
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2020-10-17 23:24:49 +0800

    Rename base package to tutorspet

---
+++ src/test/java/tutorspet/logic/commands/ClearStudentCommandTest.java
@@ -0,0 +1,33 @@
+package tutorspet.logic.commands;
+
+import static tutorspet.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tutorspet.testutil.TypicalTutorsPet.getOnlyModuleClassTutorsPet;
+import static tutorspet.testutil.TypicalTutorsPet.getTypicalTutorsPet;
+
+import org.junit.jupiter.api.Test;
+
+import tutorspet.model.Model;
+import tutorspet.model.ModelManager;
+import tutorspet.model.UserPrefs;
+
+public class ClearStudentCommandTest {
+
+    @Test
+    public void execute_emptyTutorsPet_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        expectedModel.commit(ClearStudentCommand.MESSAGE_SUCCESS);
+
+        assertCommandSuccess(new ClearStudentCommand(), model, ClearStudentCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyTutorsPet_success() {
+        Model model = new ModelManager(getTypicalTutorsPet(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalTutorsPet(), new UserPrefs());
+        expectedModel.setTutorsPet(getOnlyModuleClassTutorsPet());
+        expectedModel.commit(ClearStudentCommand.MESSAGE_SUCCESS);
+
+        assertCommandSuccess(new ClearStudentCommand(), model, ClearStudentCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
---
+++ src/test/java/tutorspet/logic/commands/ResetCommandTest.java
@@ -0,0 +1,33 @@
+package tutorspet.logic.commands;
+
+import static tutorspet.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tutorspet.testutil.TypicalTutorsPet.getTypicalTutorsPet;
+
+import org.junit.jupiter.api.Test;
+
+import tutorspet.model.Model;
+import tutorspet.model.ModelManager;
+import tutorspet.model.TutorsPet;
+import tutorspet.model.UserPrefs;
+
+public class ResetCommandTest {
+
+    @Test
+    public void execute_emptyTutorsPet_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        expectedModel.commit(ResetCommand.MESSAGE_SUCCESS);
+
+        assertCommandSuccess(new ResetCommand(), model, ResetCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyTutorsPet_success() {
+        Model model = new ModelManager(getTypicalTutorsPet(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalTutorsPet(), new UserPrefs());
+        expectedModel.setTutorsPet(new TutorsPet());
+        expectedModel.commit(ResetCommand.MESSAGE_SUCCESS);
+
+        assertCommandSuccess(new ResetCommand(), model, ResetCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
